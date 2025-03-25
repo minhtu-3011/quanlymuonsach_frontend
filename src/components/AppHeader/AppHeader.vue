f0799ed13aefa9728ce9640e42f26ead5f5f9fb4
minhtu-3011 <tudoi2002@gmail.com>
2025-03-25 16:08:01 +0700

    cai dat giao header, sidebar, va cac giao dien admin

---
+++ src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div
+      class="container-fluid"
+      style="background-color: aquamarine; height: 50px"
+    >
+      <div class="infoUser">
+        <div v-if="isUserLoggedIn()" class="contentInfo">
+          <img :src="imageUser" class="imageUser" alt="noImage" />
+          <span class="nameUser">
+            {{ username }}
+          </span>
+        </div>
+        <div v-else>
+          <router-link to="/login" class="text-decoration-none">
+            <button class="btnLogin">Đăng nhập</button>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+const isUserLoggedIn = () => {
+  return localStorage.getItem("isLoginDG") === "true";
+};
+const username = localStorage.getItem("TenDG");
+const avatar = localStorage.getItem("AvatarDG");
+const imageUser = `http://localhost:3000/${avatar}`;
+</script>
+
+<style lang="scss" scoped>
+@import "AppHeader.scss";
+</style>
---
+++ src/components/AuthHeader/AuthHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="title">
+      <div class="logo">
+        <img src="../../assets/logo2.jpg" alt="Vue" />
+        <span class="titleWeb">T<span class="text-dark">BookStore</span></span>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@import "AuthHeader.scss";
+</style>
